4a0f395e5f697b1853577236009bcd9614e1f73e
OmarSaeed-18 <omarali99940@gmail.com>
2024-12-12 16:07:29 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+console.log(5*5);
+console.log("omar saeed");
+console.log(typeof 5);
+console.log(typeof "omar");
+console.log( typeof true);
+console.log( typeof false);
+console.log(typeof 2222222224512121151n);
+
+var x=10, y=3, z=5;
+var result= x * y + z; 
+console.log(x);
+console.log(x ** y / z);
+console.log(x / z);
+console.log( result );
+
+var x=7;
+x=x+2;
+console.log(x);
+
+var x=7;
+x=x**2;
+console.log(x);
+
+var x=7;
+x-=1;
+console.log(x);
+
+var x=7 , y=12;
+result = x > y;
+console.log( result );
+
+var x=7 , y=12;
+result = x < y;
+console.log( result );
+
+var x=12 , y=12;
+result = x <= y;
+console.log( result );
+
+var x=12 , y="12";
+result = x === y;
+console.log( result );
+
+var x1="15", x2="10t" ;
+console.log(x1 - x2);
+
+var l;
+console.log(l);
